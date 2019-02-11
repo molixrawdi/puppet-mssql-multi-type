bc24981ec82c86b634fca36788047ddb919ce0f6
rawim <mo.rawi@reedbusiness.com>
2019-02-11 10:32:24 +0000

    re-applied full path to fix template

--- modules/winsql2017v3/manifests/mssql.pp
+++ modules/winsql2017v3/manifests/mssql.pp
@@ -71,7 +71,7 @@ class winsql2017v3::mssql (
 
   file { 'C:/temp/sql2017/setup.exe':
     #content => template('mssql/config.ini.erb'),  #changed by Mo Mon 11/02/2019
-    content =>template('/winsql2017v3/templates/config.ini.erb'),
+    content =>template('c:ProgramData/PuppetLabs/code/environments/test/modules/winsql2017v3/templates/config.ini.erb'),
   }
 
   exec { 'install_mssql2017':
