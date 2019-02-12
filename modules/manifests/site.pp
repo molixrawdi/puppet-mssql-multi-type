39b540d78fdc1f947fc793a0583ba140059f6e42
mo <molixrawdi>
2019-02-12 14:40:58 +0000

    added the latest ini file

--- modules/manifests/site.pp
+++ modules/manifests/site.pp
@@ -1,5 +1,5 @@
 # This is where I control the code activation from, simply via invoking the classes, higher version is the lates revision
-node 'WIN-BNUC0CKQV84' {
+node 'WIN-P2CE6DEIDNU' {
 #node 'default' {
  #  class {winsql2017v3::mssql:}
   class {winsql2017v4::mssql:}
