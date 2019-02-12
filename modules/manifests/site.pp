580aad127a7170978eb46480576984d68d3f8898
rawim <mo.rawi@reedbusiness.com>
2019-02-12 08:47:51 +0000

    Tuesday 20190212-02

--- modules/manifests/site.pp
+++ modules/manifests/site.pp
@@ -1,6 +1,6 @@
-# This is where I control the code activation from, simply via invoking the classes
+# This is where I control the code activation from, simply via invoking the classes, higher version is the lates revision
 node 'WIN-BNUC0CKQV84' {
 #node 'default' {
-   class {winsql2017v3::mssql:}
- # class {winsql2017v4::mssql:}
+ #  class {winsql2017v3::mssql:}
+  class {winsql2017v4::mssql:}
 }
